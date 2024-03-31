0d6ab03fd5786532d5ef47f92fdd799ff573cc62
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-03-31 01:37:20 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/guiestephano/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.guiestephano.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guiestephano.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order,Long>{
+
+}
