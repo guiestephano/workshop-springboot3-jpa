d9ab8c3a739a4b7d5c867ac4a86cea50bfceb1f5
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-04-03 21:02:28 -0300

    Category

---
+++ src/main/java/com/guiestephano/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.guiestephano.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guiestephano.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
