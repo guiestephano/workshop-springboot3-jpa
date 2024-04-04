45f53cc43992b2c466edb00c56448c0bce29b34d
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-04-03 22:15:24 -0300

    Product

---
+++ src/main/java/com/guiestephano/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.guiestephano.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guiestephano.course.entities.Product;
+import com.guiestephano.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
